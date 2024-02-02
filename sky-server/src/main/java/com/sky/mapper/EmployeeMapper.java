8c1f40ddfab5f4238d8a9bf574766c2862ef8dfe
haerker <haerker@foxmail.com>
2024-02-02 16:13:07 +0800

    编辑员工代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
@@ -43,5 +44,9 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
-    void startOrStop(Employee employee);
+    void update(Employee employee);
+
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
+
 }
