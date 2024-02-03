a35fc36bc291e4e68eac58f7df94e266a8bbc494
haerker <haerker@foxmail.com>
2024-02-03 17:00:22 +0800

    分类管理模块代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -2,15 +2,19 @@ package com.sky.controller.admin;
 
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
 import com.sky.properties.JwtProperties;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
+import com.sun.org.apache.regexp.internal.RE;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
@@ -23,7 +27,7 @@ public class CategoryController {
 
     @PostMapping
     @ApiOperation("新增分类")
-    public Result save(@RequestBody CategoryDTO categoryDTO) {
+    public Result<String> save(@RequestBody CategoryDTO categoryDTO) {
         categoryService.save(categoryDTO);
         return Result.success();
     }
@@ -33,5 +37,33 @@ public class CategoryController {
     public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO) {
         PageResult pageResult = categoryService.page(categoryPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    @GetMapping("/list")
+    @ApiOperation("根据类型查询分类")
+    public Result<List<Category>> list(Integer type){
+        List<Category> list = categoryService.list(type);
+        return  Result.success(list);
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用、禁用分类")
+    public Result<String> startOrStop(@PathVariable Integer status,Long id){
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    @PutMapping()
+    @ApiOperation("修改分类")
+    public Result<String> update(@RequestBody CategoryDTO categoryDTO){
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    @DeleteMapping
+    @ApiOperation("根据id删除分类")
+    public Result<String> deleteById(Long id){
+        categoryService.deleteById(id);
+        return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -4,8 +4,11 @@ import com.github.pagehelper.Page;
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
@@ -13,7 +16,14 @@ public interface CategoryMapper {
     @Insert("insert into category ( type, name, sort, status, create_time, update_time, create_user, update_user) " +
             "values " +
             "(#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
-     void insert(Category category);
+    void insert(Category category);
 
     Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    List<Category> list(Integer type);
+
+    void update(Category category);
+
+    @Delete("delete from category where id = #{id}")
+    void deleteById(Long id);
 }
