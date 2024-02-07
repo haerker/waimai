7ee5c3b04d9fb63a62630570ee1e21d5587362b9
haerker <haerker@foxmail.com>
2024-02-07 18:27:46 +0800

    套餐起售停售代码开发

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,8 +1,14 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
     void saveWithDish(SetmealDTO setmealDTO);
+
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    void startOrStop(Integer status, Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,21 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
+import com.sky.exception.SetmealEnableFailedException;
+import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +32,8 @@ public class SetmealServiceImpl implements SetmealService {
     private SetmealMapper setmealMapper;
     @Autowired
     private SetmealDishMapper setmealDishMapper;
+    @Autowired
+    private DishMapper dishMapper;
 
     @Override
     @Transactional
@@ -41,5 +53,31 @@ public class SetmealServiceImpl implements SetmealService {
            setmealDishMapper.insertBatch(setmealDishes);
        }
 
+    }
+
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(),setmealPageQueryDTO.getPageSize());
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        if(status.equals(StatusConstant.ENABLE)){
+           List<Dish> dishes = dishMapper.getBySetmealId(id);
+           if(dishes != null && !dishes.isEmpty()){
+               dishes.forEach(dish -> {
+                   if(dish.getStatus().equals(StatusConstant.DISABLE)){
+                       throw new SetmealEnableFailedException(MessageConstant.SETMEAL_ENABLE_FAILED);
+                   }
+               });
+           }
+        }
+        Setmeal setmeal = Setmeal.builder()
+                .status(status)
+                .id(id)
+                .build();
+        setmealMapper.update(setmeal);
     }
 }
